ed43847769a676e3a06ae8fcbf715fb4e746295e
zcrumpton <z.crumpton@gmail.com>
2020-10-21 18:40:00 -0500

    product class is set up

---
+++ FreakNComics/Data/ProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreakNComics.Data
+{
+    public class ProductsRepository
+    {
+
+        static List<Products> _products = new List<Products>();
+
+    }
+}
---
+++ FreakNComics/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreakNComics.Models
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+        public int ProductTypeId { get; set; }
+        public char Title {get;set;}
+        public char Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string imageUrl { get; set; }
+        public bool inStock { get; set; }
+    }
+}
