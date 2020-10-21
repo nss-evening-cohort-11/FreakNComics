b8a875d13ddb7486500983a541d4cb95cceb598e
Liza Star <lizadstar18@gmail.com>
2020-10-21 18:32:08 -0500

    set up files : model, controller, data, etc

---
+++ FreakNComics/Data/PaymentTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using FreakNComics.Models;
+namespace FreakNComics.Data
+{
+    public class PaymentTypeRepository
+    {
+        static List<PaymentType> _PaymentType = new List<PaymentType>();
+
+        const string _connectionString = "Server=localhost;Database=FreakNComics;Trusted_Connection=True;";
+    }
+}
---
+++ FreakNComics/Models/PaymentType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace FreakNComics.Models
+{
+    public class PaymentType
+    {
+        public int PaymentTypeId { get; set; }
+        public string Type { get; set; }
+        public int UserId { get; set; }
+        public int AccountNumber { get; set; }
+    }
+}
