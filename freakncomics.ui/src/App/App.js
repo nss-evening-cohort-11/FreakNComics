ebd3fd4f295050db8476f750055f4c1a141637a4
zcrumpton <z.crumpton@gmail.com>
2020-10-28 18:41:26 -0500

    wip

--- freakncomics.ui/src/App/App.js
+++ freakncomics.ui/src/App/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.scss';
 import SingleProduct from '../components/pages/SingleProduct/SingleProduct';
 import Home from '../components/pages/Home/Home';
+import MyNavbar from '../components/shared/MyNavbar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <MyNavbar />
        <SingleProduct />
        <Home />
       </div>
