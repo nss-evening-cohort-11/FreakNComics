b647a2e4aeb682b3e6aaedf61e943ec601612054
zcrumpton <z.crumpton@gmail.com>
2020-10-28 19:40:02 -0500

    Navbar setup complete. note, css unable to overwrite bootstrap css, will come back

--- freakncomics.ui/src/components/shared/MyNavbar.js
+++ freakncomics.ui/src/components/shared/MyNavbar.js
@@ -5,16 +5,26 @@ class MyNavbar extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="#">React Pinterest</a>
+      <a className="navbar-brand" href="#">Freak 'N Comics</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
+      <div className="collapse navbar-collapse d-flex justify-content-between navbarContent" id="navbarContent">
+        <ul className="navbar-nav">
           <li className="nav-item">
+            <h4><a href="#">Home</a></h4>
+          </li>
+          <li className="nav-item">
+            <h4><a href="#">Account</a></h4>
+          </li>
+          <li className="nav-item">
+            <h4><a href="#">Cart</a></h4>
           </li>
         </ul>
+        <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+      </form>
       </div>
     </nav>
     );
