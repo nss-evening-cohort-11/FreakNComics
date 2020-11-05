0bb4b9012c0a32eaf18c6f496a05eaa25ffccd8d
Liza Star <lizadstar18@gmail.com>
2020-11-04 21:00:15 -0600

    wip

--- freakncomics.ui/src/components/pages/ShoppingCart/ShoppingCart.js
+++ freakncomics.ui/src/components/pages/ShoppingCart/ShoppingCart.js
@@ -1,22 +1,58 @@
 import React from 'react';
+import PurcahseOrderData from '../../../helpers/data/PurcahseOrderData';
 import './ShoppingCart.scss';
 
 class ShoppingCart extends React.Component {
-  state = {
-    activeOrder: {},
+    state = {
+      activeOrder: {},
+      lineItems: [],
+      products: [],
+      authed: true,
+      isComplete: false,
+    }
+
+  GetShoppingCartOrderandItems = () => {
+    const userId = 3;
+    PurcahseOrderData.getCompletePurchaseOrder(userId)
+      .then((resp) => {
+        console.error(resp);
+        this.setState({ activeOrder: resp });
+        console.error(resp.purchaseOrderId);
+        PurcahseOrderData.getLineItemsByPurchaseOrderId(resp.purchaseOrderId)
+          .then((response) => {
+            console.error(response);
+            this.setState({ lineItems: [response] });
+          });
+      })
+      .catch((err) => console.error(err));
+  };
+
+  GetProductIdFromLineItem = () => {
+    const purchaseOrderId = 3;
+    PurcahseOrderData.getLineItemsByPurchaseOrderId(purchaseOrderId)
+      .then((response) => {
+        const LineItems = response.data;
+        const ArrayofLineItems = [];
+        ArrayofLineItems.push(LineItems.productId);
+        console.error(ArrayofLineItems);
+      });
+  };
+
+  componentDidMount() {
+    this.GetShoppingCartOrderandItems();
+    this.GetProductIdFromLineItem();
   }
 
   render() {
-    const { activeOrder } = this.state;
+    // const { lineItems, products } = this.state;
+
+    // const buildProductCardFromLineItem = lineItems.map((lineItem) => (
+    //   <ProductCard key={lineItem.LineItemId} lineItem={lineItem} />
+    // ));
     return (
-      <div className="activeOrder container mt-5">
+      <div className="lineItems container mt-5">
       <div className="row">
-        <div className="activeOrder-image col-8">
-          <img alt={activeOrder.title} src={activeOrder.imageUrl} />
-        </div>
-        <div className="activeOrder-details col-4">
-          <h2>{activeOrder.title} </h2>
-          <h3>${activeOrder.price}</h3>
+        <div className="lineItems-details col-4">
         </div>
       </div>
     </div>
--- freakncomics.ui/src/helpers/data/PurcahseOrderData.js
+++ freakncomics.ui/src/helpers/data/PurcahseOrderData.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 import { baseUrl } from './constants.json';
 
-const getLineItemsByPurchaseOrderId = () => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/api/orders/orderId/items`)
+const getLineItemsByPurchaseOrderId = (orderId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/orders/${orderId}/items`)
+    .then((response) => resolve(response.data))
+    .catch((err) => reject(err));
+});
+
+const getCompletePurchaseOrder = (userId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/orders/active-orders/${userId}`)
     .then((response) => resolve(response.data))
     .catch((err) => reject(err));
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getLineItemsByPurchaseOrderId };
+export default { getLineItemsByPurchaseOrderId, getCompletePurchaseOrder };
