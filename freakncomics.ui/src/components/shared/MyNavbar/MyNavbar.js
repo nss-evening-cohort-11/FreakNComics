e0b067feb22d22c296676a6c4ea29b458aae0696
Zachary Crumpton <z.crumpton@gmail.com>
2020-11-09 18:37:43 -0600

    Revert "Zc product categories bug"

--- freakncomics.ui/src/components/shared/MyNavbar/MyNavbar.js
+++ freakncomics.ui/src/components/shared/MyNavbar/MyNavbar.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import props from '../../../helpers/propz/ValueShape';
 import ProductCategoriesCollapse from '../ProductCategoriesCollapse/ProductCategoriesCollapse';
 
+// pass through the items i've searched into home.js
+
 class MyNavbar extends React.Component {
   state = {
     products: [],
@@ -34,7 +36,14 @@ class MyNavbar extends React.Component {
     this.props.history();
   }
 
+  // searchRedirect = (e) => {
+  //   e.preventDefault();
+  //   console.log(this.props.match);
+  // }
+
   render() {
+    // const { history } = this.props.match.params;
+    // console.log(history);
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="#">Freak 'N Comics</a>
