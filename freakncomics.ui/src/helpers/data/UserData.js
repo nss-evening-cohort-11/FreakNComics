e183e8b7621606036a90ee771714de89f2101c2c
John Fleming <johndeclanfleming@gmail.com>
2020-11-16 21:09:56 -0600

    set state with user order history in front end

--- freakncomics.ui/src/helpers/data/UserData.js
+++ freakncomics.ui/src/helpers/data/UserData.js
@@ -7,5 +7,11 @@ const getUserByUserId = (userId) => new Promise((resolve, reject) => {
     .catch((err) => reject(err));
 });
 
+const getCompletedOrdersByUserId = (userId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/orders/order-history/${userId}`)
+    .then((response) => resolve(response.data))
+    .catch((err) => reject(err));
+});
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getUserByUserId };
+export default { getUserByUserId, getCompletedOrdersByUserId };
