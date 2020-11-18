333bd1abd84fd510640a1dc889a44cd0c9bdc917
Liza Star <lizadstar18@gmail.com>
2020-11-17 19:16:08 -0600

    wip to checkout branch

---
+++ freakncomics.ui/src/components/pages/SingleOrderDetail/SingleOrderDetail.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './SingleOrderDetail.scss';
+
+class SingleOrderDetail extends React.Component {
+  render() {
+    return (
+      <div className="SingleOrderDetail">
+        Order Details
+      </div>
+    );
+  }
+}
+export default { SingleOrderDetail };
