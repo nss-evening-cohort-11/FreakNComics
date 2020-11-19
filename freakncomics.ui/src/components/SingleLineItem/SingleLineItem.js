d8b51988c3ac6063e1aa0677b7e2b150ad3828da
Liza Star <lizadstar18@gmail.com>
2020-11-18 21:15:06 -0600

    wip

--- freakncomics.ui/src/components/SingleLineItem/SingleLineItem.js
+++ freakncomics.ui/src/components/SingleLineItem/SingleLineItem.js
@@ -17,29 +17,32 @@ class SingleLineItem extends React.Component {
       const { lineItem } = this.props;
 
       return (
-      <div className="SingleLineItem container">
-        <div className="row">
-          <h5>
-         Product:
-          </h5>
-          <div className="col">
-          {lineItem.title}
-          </div>
-          <h5>
-          Unit Price:
-          </h5>
-          <div className="col">
-          ${lineItem.unitPrice}
-          </div>
-         <h5>
-         Quantity:
-         </h5>
-          <div className="col">
-          {lineItem.lineItemQuantity}
-          </div>
-          <button className="remove-item-btn btn btn-danger mb-2" onClick={this.removingLineItem}> <i className="fas fa-trash"></i> </button>
+        // eslint-disable-next-line react/style-prop-object
+        <div className="card border-dark mb-3">
+        <div className="card-header"> <h3 className="text-center"><strong>Product: </strong> </h3>   <h5 className="text-center" >{lineItem.title}</h5> </div>
+        <div className="card-body text-dark">
+        <h5 className="card-title text-center"> ${lineItem.unitPrice} </h5>
+        <p className="card-text text-center">  Quantity: {lineItem.lineItemQuantity} </p>
         </div>
-      </div>
+        <button className="remove-item-btn btn btn-dark mb-2 align-items-center col-4 offset-4" onClick={this.removingLineItem}> <i className="fas fa-trash"></i> </button>
+        </div>
+      // <div className="SingleLineItem container">
+      //   <div className="row">
+      //     <h5> Product: </h5>
+      //     <div className="col">
+      //     {lineItem.title}
+      //     </div>
+      //     <h5> Unit Price: </h5>
+      //     <div className="col">
+      //        ${lineItem.unitPrice}
+      //     </div>
+      //    <h5> Quantity:</h5>
+      //     <div className="col">
+      //      {lineItem.lineItemQuantity}
+      //     </div>
+
+      //   </div>
+      // </div>
       );
     }
 }
